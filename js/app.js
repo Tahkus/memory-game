95230b594f265b21b036ca21cfe6e8a73e539286
Tahkus-Allen <tahkus.ekedal@gmail.com>
2018-05-21 14:27:34 -0700

    Added responsive restart button, refresh, clickedCard

--- js/app.js
+++ js/app.js
@@ -1,14 +1,24 @@
 /*
  * Create a list that holds all of your cards
  */
+// const cardSymbols = ['fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-cube', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-bomb', 'fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-cube', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-bomb']
+let card = document.getElementsByClassName('card')
+let cards = [...card]
+let deck = document.querySelector('.deck')
+let movesNumber = document.querySelector('.moves')
+let matchedCardNum = 0
+let matchedCards = []
+let openCardsList = []
+let timer = document.querySelector('.timer')
+let time = '00:00'
+let seconds = 0
+let minutes = 0
+let hours = 0
+let moves = 0
+let shuffledCards = shuffle(cards)
+let initialClick = 0
+const restartButton = document.querySelector('.restart')
 
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -25,14 +35,68 @@ function shuffle(array) {
     return array;
 }
 
+// FUNCTIONS
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+// Reset game when window refreshes/loads
+window.onload = newGame()
+
+// Change html of cards to shuffled deck
+function rearrangeCards(cards) {
+  for (let i = 0; i < shuffledCards.length; i++) {
+    deck.appendChild(shuffledCards[i]);
+    shuffledCards[i].classList.remove('open', 'show', 'match');
+  }
+};
+
+// Reset game to begin fresh
+function newGame(cards) {
+  rearrangeCards();
+  moves = 0;
+  matchedCards = [];
+  matchedCardNum = 0;
+  openCardsList = [];
+  timer.innerHTML = '00:00';
+  document.querySelector('.one').innerHTML = '<i class="fa fa-star"></i>';
+  document.querySelector('.two').innerHTML = '<i class="fa fa-star"></i>';
+  document.querySelector('.three').innerHTML = '<i class="fa fa-star"></i>';
+};
+
+// Things that happen when a card is clicked on
+function cardClicked(event) {
+  openCardsList.push(this);
+  this.classList.add('open', 'show');
+  if (openCardsList.length === 2 && openCardsList[0].innerHTML === openCardsList[1].innerHTML) {
+    match();
+  }
+  if (openCardsList.length === 2 && openCardsList[0].innerHTML != openCardsList[1].innerHTML) {
+    noMatch();
+  }
+};
+
+// Matching cards function
+function match() {
+    matchedCards.push(openCardsList[0]);
+    matchedCards.push(openCardsList[1]);
+    openCardsList[0].classList.add('match');
+    openCardsList[1].classList.add('match');
+    openCardsList[0].classList.remove('open', 'show');
+    openCardsList[1].classList.remove('open', 'show');
+    openCardsList = [];
+};
+
+// No-match function
+function noMatch() {
+    openCardsList[0].classList.remove('open', 'show');
+    openCardsList[1].classList.remove('open', 'show');
+    openCardsList = [];
+};
+
+// EVENT LISTENERS
+restartButton.addEventListener('click', function() {
+  shuffle(cards)
+  newGame();
+});
+
+for (let shuffledCard of shuffledCards) {
+  shuffledCard.addEventListener('click', cardClicked);
+};
